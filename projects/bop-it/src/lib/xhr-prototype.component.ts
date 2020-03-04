10b0c4b794d813502e8c8098466bed973ab692c4
Tim Neil <tim.neil@reedbusiness.com>
2020-03-04 15:12:18 +0000

    - Testing slots

--- projects/bop-it/src/lib/xhr-prototype.component.ts
+++ projects/bop-it/src/lib/xhr-prototype.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'xhr-prototype',
   template: `
+    <slot (slotchange)="slotChange($event)">Default heading</slot>
     <p>
       Prototype works! Hello
     </p>
   `,
-  styles: []
+  styles: [],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class XhrPrototypeComponent implements OnInit {
 
@@ -16,4 +18,8 @@ export class XhrPrototypeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  slotChange($event: any) {
+    const assigned = $event.target.assignedNodes();
+    console.log(assigned);
+  }
 }
